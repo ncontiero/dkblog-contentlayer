ead839779595b041c949127a27c157935d4c6016
DKSHS <88355080+dkshs@users.noreply.github.com>
2023-09-28 16:30:16 -0300

    Initial commit from `cookiecutter-nextjs`

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DkBlog",
+  description: "A blog using Next.Js and Contentlayer.",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
