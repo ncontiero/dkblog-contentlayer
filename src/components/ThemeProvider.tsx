f4627f228a0835cc5454cdd6aef21c26a63fe930
DKSHS <88355080+dkshs@users.noreply.github.com>
2023-10-02 11:19:14 -0300

    update: removing unused react import

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import * as React from "react";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import type { ThemeProviderProps } from "next-themes/dist/types";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ ...props }: ThemeProviderProps) {
+export function ThemeProvider(props: ThemeProviderProps) {
   return <NextThemesProvider {...props} />;
 }
